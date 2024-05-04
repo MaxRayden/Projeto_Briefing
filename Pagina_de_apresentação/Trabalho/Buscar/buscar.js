cb92b9b01f739359af426a40c5e862a6bdd377b0
Edwardo-Magalhaes <eduardomagalhaes.pro.01@gmail.com>
2024-05-03 20:26:10 -0400

    teste

--- Pagina_de_apresentação/Trabalho/Buscar/buscar.js
+++ Pagina_de_apresentação/Trabalho/Buscar/buscar.js
@@ -1,10 +1,17 @@
-// Verifica se há dados no Local Storage
-if(localStorage.getItem('dados')) {
-    // Se houver dados, recupera e converte para objeto JavaScript
-    const dados = JSON.parse(localStorage.getItem('dados'));
+document.addEventListener("DOMContentLoaded", function() {
+    const listaDados = document.getElementById('lista-dados');
     
-    // Faça o que você precisa fazer com os dados aqui
-    console.log('Dados recuperados do Local Storage:', dados);
-} else {
-    console.log('Não há dados no Local Storage.');
-}
+    // Limpa a lista de dados
+    listaDados.innerHTML = '';
+
+    // Itera sobre todas as chaves do Local Storage
+    for (let i = 0; i < localStorage.length; i++) {
+        const chave = localStorage.key(i);
+        const valor = localStorage.getItem(chave);
+        
+        // Cria um item de lista para cada chave-valor e adiciona à lista na tela
+        const itemLista = document.createElement('li');
+        itemLista.textContent = `${chave}: ${valor}`;
+        listaDados.appendChild(itemLista);
+    }
+});
